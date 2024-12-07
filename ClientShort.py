721f520068c8db1dd9ae35cbd7c25588976745bd
NikShaydarov <112931863+NikShaydarov@users.noreply.github.com>
2024-12-07 20:29:42 +0300

    LAB 1: ADDING ID

--- ClientShort.py
+++ ClientShort.py
@@ -1,7 +1,7 @@
-import re
-
 class ClientShort:
-    def __init__(self, name, surname, phone):
+    def __init__(self, name, surname, phone, id=None):
+        if id is not None:
+            self.set_id(id)
         self.set_name(name)
         self.set_surname(surname)
         self.set_phone(phone)
@@ -21,6 +21,21 @@ class ClientShort:
         if not re.match(r'^\+?\d{10,15}$', phone):
             return False  # Если телефон не соответствует формату
         return True
+
+    # Валидация целого числа (неотрицательное)
+    @staticmethod
+    def validate_int(value):
+        if not isinstance(value, int):
+            return False  # Убедитесь, что значение - целое число
+        if value < 0:
+            return False  # Если сумма покупки отрицательная
+        return True
+
+    def set_id(self, id):
+        if self.validate_int(id):
+            self.id = id
+        else:
+            raise ValueError("ID не может быть отрицательным и должен быть целым числом.")
 
     # Сеттер для имени с валидацией
     def set_name(self, name):
@@ -50,12 +65,16 @@ class ClientShort:
     def get_surname(self):
         return self.surname
 
+    def get_id(self):
+        return getattr(self, 'id', None)  # Используем getattr, чтобы избежать AttributeError
+
     def get_phone(self):
         return self.phone
 
     # Строковое представление для вывода
     def __str__(self):
-        return f"ClientShort(name='{self.name}', surname='{self.surname}', phone='{self.phone}')"
+        return (f"ClientShort(id={self.get_id()}, name='{self.get_name()}', "
+                f"surname='{self.get_surname()}', phone='{self.get_phone()}')")
 
     # Переопределение equals (сравнение объектов по телефону)
     def __eq__(self, other):
